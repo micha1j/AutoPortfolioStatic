0ef90cf1cd966d049cbaa99cf493397ce19c5a35
Michal Chrapowicki <michal333878@gmail.com>
2025-08-03 01:37:32 +0200

    dodana funkcjonalność 2

--- script.js
+++ script.js
@@ -9,13 +9,28 @@ fetch(`https://api.github.com/users/${githubUsername}/repos`).then(response => {
 	return response.json()
 })
 .then(repos => {
-	repos.forEach(repo => {
-		const repoElement = document.createElement("div");
+	function formatDate(dateString) {
+  const date = new Date(dateString);
+  return date.toLocaleDateString("pl-PL", {
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+	});
+	}
+	repos.filter(repo => {
+    const lastUpdated = new Date(repo.updated_at);
+    return lastUpdated.getFullYear() >= 2025;
+  })
+.sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at))
+	
+.forEach(repo => {
+		const repoElement = document.createElement("li");
 		repoElement.classList.add("repo");
 		
 		repoElement.innerHTML = `
 		<h2><a href="${repo.html_url}" target="_blank">${repo.name}</a></h2>
 		<p>${repo.description || "Brak opisu"}</p>
+		<p>${formatDate(repo.updated_at)}</p>
 		`;
 		
 		repoList.appendChild(repoElement);
